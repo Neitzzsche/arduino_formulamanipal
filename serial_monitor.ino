26ad9c9de8b99e21dbe03fa985f326d20fd0cf96
Neitzzsche <155979441+Neitzzsche@users.noreply.github.com>
2024-05-20 23:44:38 +0530

    Add files via upload

---
+++ serial_monitor.ino
@@ -0,0 +1,22 @@
+const int ledPin = 13;
+
+void setup() {
+  
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+}
+
+void loop() {
+  if (Serial.available()) {
+    char receivedChar = Serial.read(); 
+    if (receivedChar == '1') {
+      digitalWrite(ledPin, HIGH); 
+      Serial.println("LED turned ON");
+    } else if (receivedChar == '0') {
+      digitalWrite(ledPin, LOW); 
+      Serial.println("LED turned OFF");
+    }
+  }
+  
+}
